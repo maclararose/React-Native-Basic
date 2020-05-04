488a0de84d0d7d8b94853510a756b1664caf6a15
maclararose <maria.uespi@outlook.com>
2020-05-03 21:16:13 -0300

    Modified My-App

--- My-App/App.js
+++ My-App/App.js
@@ -4,23 +4,42 @@ import { View, Text, Image } from 'react-native';
 class App extends Component{
   render(){
     let nome = 'Maria';
+    let img = 'https://sujeitoprogramador.com/steve.png';
 
     return(
       <View>
         <Text>Olá Mundo!</Text>
+
         <Text>Meu Primeiro App</Text>
+
         <Text style={{ color:'#FF0000', fontSize: 20, margin: 15 }}>
           Alternativa Nerd
         </Text>
 
-        <Image
-          source={{uri: 'https://sujeitoprogramador.com/steve.png' }}
-          style={{width: 300, height: 300}}
-        />
-
         <Text style={{ fontSize: 30, color: 'blue' }}>{nome}</Text>
 
+        <Jobs largura={500} altura={200} nome="Stte JOBS"/>
+
       </View>
+    );
+  }
+}
+
+class Jobs extends Component{
+  render(){
+
+    let img = 'https://sujeitoprogramador.com/steve.png';
+
+    return(
+      <View>
+        <Image 
+          source={{ uri: img }}
+          style={{ width:this.props.largura, height:this.props.altura }}
+        />
+
+    <Text style={{ fontSize: 30 }}>{this.props.nome}</Text>
+      </View>
+
     );
   }
 }
